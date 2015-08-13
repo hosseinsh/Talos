9af3050467a8574ab81d1a29807fbb8cfcbc57c8
Hossein Shafagh <shafagh@inf.ethz.ch>
2015-08-13 15:04:27 -0700

    ECC curves

--- cpu/cc2538/dev/ecc-curve.h
+++ cpu/cc2538/dev/ecc-curve.h
@@ -47,30 +47,16 @@
 #ifndef ECC_CURVE_H_
 #define ECC_CURVE_H_
 
-//[NIST P-256, X9.62 prime256v1]
-static uint32_t nist_p_256_p[8] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0x00000000,
-                                    0x00000000, 0x00000000, 0x00000001, 0xFFFFFFFF };
-static uint32_t nist_p_256_n[8] = { 0xFC632551, 0xF3B9CAC2, 0xA7179E84, 0xBCE6FAAD,
-                                    0xFFFFFFFF, 0xFFFFFFFF, 0x00000000, 0xFFFFFFFF };
-static uint32_t nist_p_256_a[8] = { 0xFFFFFFFC, 0xFFFFFFFF, 0xFFFFFFFF, 0x00000000,
-                                    0x00000000, 0x00000000, 0x00000001, 0xFFFFFFFF };
-static uint32_t nist_p_256_b[8] = { 0x27D2604B, 0x3BCE3C3E, 0xCC53B0F6, 0x651D06B0,
-                                    0x769886BC, 0xB3EBBD55, 0xAA3A93E7, 0x5AC635D8 };
-static uint32_t nist_p_256_x[8] = { 0xD898C296, 0xF4A13945, 0x2DEB33A0, 0x77037D81,
-                                    0x63A440F2, 0xF8BCE6E5, 0xE12C4247, 0x6B17D1F2 };
-static uint32_t nist_p_256_y[8] = { 0x37BF51F5, 0xCBB64068, 0x6B315ECE, 0x2BCE3357,
-                                    0x7C0F9E16, 0x8EE7EB4A, 0xFE1A7F9B, 0x4FE342E2 };
+/*
+ * NIST P-256, X9.62 prime256v1
+ */
+extern ecc_curve_info_t nist_p_256;
 
-static ecc_curve_info_t nist_p_256 = {
-  .name       = "NIST P-256",
-  .ui8Size    = 8,
-  .pui32Prime = nist_p_256_p,
-  .pui32N     = nist_p_256_n,
-  .pui32A     = nist_p_256_a,
-  .pui32B     = nist_p_256_b,
-  .pui32Gx    = nist_p_256_x,
-  .pui32Gy    = nist_p_256_y
-};
+/*
+ * NIST P-192, X9.62
+ */
+extern ecc_curve_info_t nist_p_192;
+
 
 #endif /* CURVE_INFO_H_ */
 
